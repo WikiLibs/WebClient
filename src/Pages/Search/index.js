be9818d6eb3ca6ad48f36a7b0ea4aa507990c753
Samuel <samuel.rousseaux@epitech.eu>
2020-01-20 17:55:01 +0900

    Reworked searchpage

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,83 +1,128 @@
 import React, { Component } from 'react';
 import ApiService from '../../ApiService';
-import {Card, Button, Container } from 'react-bootstrap';
+import Button from '@material-ui/core/Button'
+
+import './style.css'
 
 export default class Search extends Component {
     api = new ApiService();
 
     constructor(props) {
         super(props);
-        this.handleNext = this.handleNext.bind(this);
-        this.handlePrev = this.handlePrev.bind(this);
+        this.state = {
+            data: null,
+            hasMorePages: false,
+            page: 1,
+            symbols: {}
+        };
     }
 
-    renderSymList() {
-        var vals = []
-        if (this.state == null || this.state.data.length <= 0)
-            return (<h1 className='header1'>No results found</h1>);
-        for (var v in this.state.data) {
-            if (this.state.data[v].type === "attribute" || this.state.data[v].type === "member")
-                continue;
-            vals.push(
-                <div key={v} className='paddingWell'>
-                    <Card className="card-inverse paddingCard" style={{ backgroundColor: 'lightgray', borderColor: 'lightgrey' }}>
-                        <Card.Body>
-                            <Card.Text className="black">
-                                <a href={"/symbol/" + this.state.data[v].id}><span className='func'>{this.state.data[v].path}</span></a>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-            );
-        }
-        return (vals);
-    }
 
-    handleNext() {
-        ++this.page;
+    componentDidMount = () => {
         this.refrehData();
     }
 
-    handlePrev() {
-        --this.page;
+    refrehData = () => {
+        this.api.searchSymbols(this.props.match.params.path, this.state.page).then(response => {
+            this.sortResultsIntoList(response.data)
+        });
+    }
+
+    handleNext = () => {
+        this.setState({'page': this.state.page + 1})
         this.refrehData();
     }
 
-    renderFooter() {
-        if (this.state == null || this.state.data.length <= 0)
+    handlePrev = () => {
+        this.setState({'page': this.state.page - 1})
+        this.refrehData();
+    }
+
+    sortResultsIntoList = (data) => {
+        let symbolsTemp = data.data
+        let symbolsState = this.state.symbols
+
+        symbolsTemp.forEach((sym) => {
+            if (!(sym.type in symbolsState))
+                symbolsState[sym.type] = []
+            symbolsState[sym.type].push(sym)
+        })
+
+        this.setState({
+            'symbols': symbolsState,
+            'hasMorePages': data.hasMorePages
+        })
+    }
+
+    renderFooter = () => {
+        if (Object.keys(this.state.symbols).length === 0)
             return;
         if (!this.state.hasMorePages && this.page > 1)
             return (
-                <Button onClick={this.handlePrev}>Previous page</Button>
-            );
+                <Button variant="contained" onClick={this.handlePrev} classes={{root: 'button-style', label:'button-style'}}>
+                    Previous page
+                </Button>
+            )
         if (this.page > 1)
             return (
                 <div>
-                    <Button onClick={this.handlePrev}>Previous page</Button>
-                    <Button onClick={this.handleNext}>Next page</Button>
+                    <Button variant="contained" onClick={this.handlePrev} classes={{root: 'button-style', label:'button-style'}}>
+                        Previous page
+                    </Button>
+                    <Button variant="contained" onClick={this.handleNext} classes={{root: 'button-style', label:'button-style'}}>
+                        Next page
+                    </Button>
                 </div>
-            );
+            )
         else
-            return (<Button onClick={this.handleNext}>Next page</Button>);
+            return (
+                <Button variant="contained" onClick={this.handleNext} classes={{root: 'button-style', label:'button-style'}}>
+                    Next page
+                </Button>
+            )
     }
 
-    render() {
+    renderSymbolList = () => {
         return (
-            <Container>
-                <h1 className='header1'>Results for '{this.props.match.params.path}'</h1>
-                <p></p>
-                {this.renderSymList()}
+            <div>
+                {Object.keys(this.state.symbols).map((key) =>
+                    <div className='search-page-results-container'>
+                        <div className='search-page-title'>
+                            {key[0].toUpperCase() + key.slice(1)}
+                            <span className='search-page-title-number'>{this.state.symbols[key].length} result(s)</span>
+                        </div>
+                        {this.state.symbols[key].map((symbol) =>
+                            <a className='override-a' href={'/symbol/' + symbol.id}>
+                                <div className='search-page-result-container'>
+                                    <div className='search-page-result-title'>
+                                        {symbol.path}
+                                    </div>
+                                    <div className='search-page-result-description'>
+                                        Description unavailable yet.
+                                    </div>
+                                    <div className='search-page-separator'/>
+                                    <div className='search-page-result-bottom-container'>
+                                        <div>Last update : {(new Date(symbol.lastModificationDate)).toLocaleDateString} (by: {symbol.userName})</div>
+                                        <div>Viewed {symbol.views} times(s)</div>
+                                    </div>
+                                </div>
+                            </a>
+                        )}
+                    </div>
+                )}
+            </div>
+        )
+    }
+
+    render = () => {
+        return (
+            <div className='search-page-container'>
+                <div className='search-page-title'>
+                    Results for : '{this.props.match.params.path}'
+                </div>
+                {this.renderSymbolList()}
                 {this.renderFooter()}
-            </Container>
+            </div>
         );
-    }
-
-    refrehData() {
-        this.api.searchSymbols(this.props.match.params.path, this.page).then(response => { this.setState(response.data); });
-    }
-
-    componentDidMount() {
-        this.page = 1;
-        this.refrehData();
     }
 }
