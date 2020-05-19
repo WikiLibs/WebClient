33f9b1f194350515e259e5e8b92801107ebed525
Nicolas Jacson <nicolas.jacson@epitech.eu>
2020-05-19 21:20:24 +0900

    adding how to use parser page

---
+++ src/Pages/HowToUseParser/HowToUseParser.component.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import './style.css';
+
+import welcome from './img/welcome_lib_uploader.png'
+import uploading from './img/uploading.png'
+import uploading2 from './img/uploading2.png'
+import successful from './img/upload_successful.png'
+import summary from './img/summary.png'
+import inputInfo from './img/input_info_complete.png'
+
+const renderHowToUseDesc = () => {
+    return (
+        <div className="howtouseparser_card">
+            <h2 className="howtouseparser_title">How to use the parser</h2>
+            <div className="howtouseparser_content">
+                <p className="howtouseparser_description">
+                    To upload a new library to the website, you will need to use our program which is quite easy to use.<br />
+                    Here are the steps.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const renderTuto = () => {
+    return (
+        <div className="howtouseparser_card">
+            <h2 className="howtouseparser_title">Walkthrough</h2>
+            <div className="howtouseparser_inner_content">
+                <div className="howtouseparser_inner_content_card">
+                    <p className="howtouseparser_secondary_title">Starting page</p>
+                    <img className="howtouseparser_screenshot" src={welcome} alt=""/>
+                </div>
+                <div className="howtouseparser_inner_content_card">
+                    <p className="howtouseparser_secondary_title">Fill in the infos</p>
+                    <p className="howtouseparser_secondary_desc">In this step, you need to fill the language used in your library (the program will only look for this language), the library name (no special characters) and the location of the library (choose the root).</p>
+                    <img className="howtouseparser_screenshot" src={inputInfo} alt=""/>
+                </div>
+                <div className="howtouseparser_inner_content_card">
+                    <p className="howtouseparser_secondary_title">Summary</p>
+                    <p className="howtouseparser_secondary_desc">Verify that the informations you filled are correct, then press the button.</p>
+                    <img className="howtouseparser_screenshot" src={summary} alt=""/>
+                </div>
+                <div className="howtouseparser_inner_content_card">
+                    <p className="howtouseparser_secondary_title">Processing and upload</p>
+                    <p className="howtouseparser_secondary_desc">Just wait for the processing and upload to finish.</p>
+                    <img className="howtouseparser_screenshot" src={uploading} alt=""/>
+                    <img className="howtouseparser_screenshot" src={uploading2} alt=""/>
+                </div>
+                <div className="howtouseparser_inner_content_card">
+                    <p className="howtouseparser_secondary_title">Finished</p>
+                    <p className="howtouseparser_secondary_desc">The upload has now ended, you can finally close the window ! Some more processing time may be required on our side, so please be patient !</p>
+                    <img className="howtouseparser_screenshot" src={successful} alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const HowToUseParser = () => {
+    return (
+        <div>
+            <div id="Body">
+                <div className="content_howtouseparser_page">
+                    {renderHowToUseDesc()}
+                    {renderTuto()}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HowToUseParser;
