eaa51f8f13c267641eed013bf7462a8a2a067591
William Tessari <william.tessari@epitech.eu>
2020-09-22 16:28:19 +0200

    maj

--- src/Pages/LibPage/index.js
+++ src/Pages/LibPage/index.js
@@ -2,44 +2,70 @@ import React, { Component } from 'react';
 import { useQuery } from '../../util';
 import { ApiService } from '../../ApiService';
 
+import Tree from 'react-tree-graph';
+
+import "./style.css";
+
 export default class LibPage extends Component {
-api = new ApiService();
+    api = new ApiService();
 
     state = {
-        data: [],
-        libId: -1
-    };
+        data: {},
+        listType: [],
+        libId: -1,
+        libName: ""
+      };
+    
+    onClick = (event, nodeKey) => {
+        alert(`Left clicked ${nodeKey}`);
+    }
+
+    setTree = () => {
+
+        return(
+            {
+                name: this.state.libName,
+                children: []
+            }
+        );
+    }
 
     render = () => {
-        let vals = []
-        if (this.state.data) {
-            this.state.data.forEach(elem => {
-                vals.push(
-                    <p key={elem.id} >{"ID: " + elem.id + ", Path: " + elem.path}</p>
-                );
-            })
-        }
-        return(
-            <div>
-                <h1>{"library ID:" + this.state.libId}</h1>
-                <h2>{vals}</h2>
+        let tree = this.setTree();
+        return (
+            <div className="custom-container">
+                <Tree
+	                data={tree}
+	                height={200}
+	                width={400}
+                    animated
+                    duration={1200}
+                    svgProps={{
+                        className: 'custom'
+                    }}
+                    gProps={{
+                        onClick: this.onClick,
+                    }}
+	            />
             </div>
         );
     }
 
     componentDidMount = async () => {
-        var q = useQuery();
-        let query = {
-            page: 1,
-            count: 10,
-            lib: null,
-            lang: null,
-            type: null,
-            path: null
-        };
-        query.lib = parseInt(q.lib);
-        this.setState({libId:query.lib});
-            
-        this.api.searchSymbols(query).then(response => this.setState({data:response.data.data}))
-    }
+        var q = useQuery();        
+        let listData = {};
+        let listType = [];
+
+        this.api.getInfoTree(parseInt(q.lib)).then(response => {
+            response.data.forEach(elem => {
+                if (!listData[elem.type]) {
+                    listData[elem.type] = [];
+                    listType.push(elem.type);
+                }
+                listData[elem.type].push(elem);
+            })
+        });
+        console.log(listData);
+        this.setState({data: listData, listType: listType, libId: parseInt(q.lib), libName: q.name});
+    };
 }
