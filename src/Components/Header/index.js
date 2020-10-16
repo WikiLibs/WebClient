75119044d2765029387dfe257f44c169c12d34f0
Yuri Edward <yuri6037@outlook.com>
2020-10-16 19:25:05 +0200

    Refactor remove old code

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -19,7 +19,6 @@ import SearchIcon from "@material-ui/icons/Search";
 import './index.css'
 import 'react-dropdown/style.css';
 
-import pp from './pp.png'
 import AccountButton from './AccountButton';
 
 var suggestions = [];
@@ -217,22 +216,6 @@ class Header extends Component {
     ////////////////////////////////////////////////////////////////////////
 
     checkConnect() {
-        //if (this.props.user) {
-        //    return (<div className="header-right">
-        //        {/* {this.props.user.hasPermission("user.me.update") && <Link style={{ color: "white", padding: "24px" }} to='/profile'>My Profile</Link>} */}
-        //        <Link to="/download">Download</Link>
-        //        <Link to='/admin'>Administration</Link>
-        //        <Link onClick={() => this.api.disconnect()} to='/'>Disconnect</Link>
-        //        {this.props.user.hasPermission("user.me.update") && <Link to="/profile"><img alt="" src={pp} className="header-img-profile"></img></Link>}
-        //    </div>);
-        //} else {
-        //    return (<div className="header-right">
-        //        {/* <Link style={{ color: "white", padding: "24px" }} to='/usercreation'>Create account</Link> */}
-        //        <Link to='/userconnection'>LOGIN</Link>
-        //        <Link to="/faq">FAQ</Link>
-        //        <Link to="/contact">CONTACT</Link>
-        //    </div>);
-        //}
         return (
             <>
                 {
